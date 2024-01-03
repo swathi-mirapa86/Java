2938b8266663647073ed6921bcbd00d5192897c0
swathi-mirapa86 <swathimirapa86@gmail.com>
2024-01-03 08:27:14 +0530

    First commit

---
+++ Altek.java
@@ -0,0 +1,16 @@
+package mmmm;
+
+public class Altek {
+	public static void main(String[] args) {
+		
+		int i;
+		i=1;
+		 while(i<=5)
+		 {
+			 System.out.println(i + ".ALTEK" );
+			 i++;
+		 }
+
+	}
+
+}
